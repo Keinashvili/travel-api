36f45d4baf41cccfface04d8644f51d33927dd86
Vladimer Keinashvili <vovakeinashvili@gmail.com>
2023-08-14 00:50:35 +0400

    added users into UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,31 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@dev.com',
+            'password' => Hash::make('admin'),
+        ]);
+
+        User::create([
+            'name' => 'editor',
+            'email' => 'editor@dev.com',
+            'password' => Hash::make('editor'),
+        ]);
+
+
+        User::create([
+            'name' => 'client',
+            'email' => 'client@dev.com',
+            'password' => Hash::make('client'),
+        ]);
     }
 }
