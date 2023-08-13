43a1f2e38005b5602672cf3514e04933f666815c
Vladimer Keinashvili <vovakeinashvili@gmail.com>
2023-08-14 00:54:39 +0400

    feat: added forgotten roles to users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -10,23 +11,28 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
+        $admin = User::create([
             'name' => 'admin',
             'email' => 'admin@dev.com',
             'password' => Hash::make('admin'),
         ]);
 
-        User::create([
+        $admin->roles()->attach(Role::where('name', 'admin')->value('id'));
+
+        $editor = User::create([
             'name' => 'editor',
             'email' => 'editor@dev.com',
             'password' => Hash::make('editor'),
         ]);
 
+        $editor->roles()->attach(Role::where('name', 'editor')->value('id'));
 
-        User::create([
+        $client = User::create([
             'name' => 'client',
             'email' => 'client@dev.com',
             'password' => Hash::make('client'),
         ]);
+
+        $client->roles()->attach(Role::where('name', 'client')->value('id'));
     }
 }
