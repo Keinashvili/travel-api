746db777c178df533041af3a34af3cf99152bb33
Vladimer Keinashvili <vovakeinashvili@gmail.com>
2023-08-13 19:20:01 +0400

    Added creating tours for admin

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\Admin\{TravelController};
+use App\Http\Controllers\Api\V1\Admin\{TourController, TravelController};
 
 Route::controller(TravelController::class)->group(function () {
     Route::post('/travel/store', 'store');
+});
+
+Route::controller(TourController::class)->group(function () {
+    Route::post('/travel/{travel:id}/tour/store', 'store');
 });
